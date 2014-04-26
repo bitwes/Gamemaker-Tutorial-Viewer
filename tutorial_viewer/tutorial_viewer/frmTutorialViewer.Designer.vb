c187fda9fc1fbe77e65f788cb7e41b26a955f0c2
bitwes <butch.wesley@gmail.com>
2014-04-26 18:48:30 -0400

    Initial Set of files

---
+++ tutorial_viewer/tutorial_viewer/frmTutorialViewer.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTutorialViewer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtProjectFolder = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.webTutorial = New System.Windows.Forms.WebBrowser()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.lblCurPage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Project Folder"
+        '
+        'txtProjectFolder
+        '
+        Me.txtProjectFolder.Location = New System.Drawing.Point(90, 16)
+        Me.txtProjectFolder.Name = "txtProjectFolder"
+        Me.txtProjectFolder.Size = New System.Drawing.Size(457, 20)
+        Me.txtProjectFolder.TabIndex = 1
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(562, 14)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 2
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'webTutorial
+        '
+        Me.webTutorial.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.webTutorial.Location = New System.Drawing.Point(-1, 62)
+        Me.webTutorial.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.webTutorial.Name = "webTutorial"
+        Me.webTutorial.Size = New System.Drawing.Size(799, 604)
+        Me.webTutorial.TabIndex = 3
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(643, 14)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(67, 23)
+        Me.btnGo.TabIndex = 4
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnBack.Location = New System.Drawing.Point(635, 672)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(75, 23)
+        Me.btnBack.TabIndex = 5
+        Me.btnBack.Text = "Back"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnNext.Location = New System.Drawing.Point(724, 672)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(64, 23)
+        Me.btnNext.TabIndex = 6
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'lblCurPage
+        '
+        Me.lblCurPage.AutoSize = True
+        Me.lblCurPage.Location = New System.Drawing.Point(695, 46)
+        Me.lblCurPage.Name = "lblCurPage"
+        Me.lblCurPage.Size = New System.Drawing.Size(93, 13)
+        Me.lblCurPage.TabIndex = 7
+        Me.lblCurPage.Text = "No Pages Loaded"
+        '
+        'frmTutorialViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 707)
+        Me.Controls.Add(Me.lblCurPage)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.webTutorial)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtProjectFolder)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmTutorialViewer"
+        Me.Text = "Game Maker Tutorial Viewer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtProjectFolder As System.Windows.Forms.TextBox
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents webTutorial As System.Windows.Forms.WebBrowser
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+    Friend WithEvents btnBack As System.Windows.Forms.Button
+    Friend WithEvents btnNext As System.Windows.Forms.Button
+    Friend WithEvents lblCurPage As System.Windows.Forms.Label
+
+End Class
---
+++ tutorial_viewer/tutorial_viewer/frmTutorialViewer.vb
@@ -0,0 +1,121 @@
+﻿Imports System.Text
+Imports System.Xml
+Imports System.IO
+Imports System.IO.TextReader
+
+Public Class frmTutorialViewer
+    Dim g_tutorialPages() As String = {}
+    Dim g_currentPage As Integer = 0
+
+    Sub loadEverything()
+        Dim gmxFileName As String = getGMXFileName(txtProjectFolder.Text)
+        Debug.Print("gmx file = " + gmxFileName)
+        If (gmxFileName <> "NA") Then
+            Dim tutorialPath As String = getTutorialFolder(gmxFileName)
+            If (tutorialPath <> "") Then
+                loadTutorialPages(tutorialPath)
+            End If
+        Else
+            MessageBox.Show("Could not find a project file in " + txtProjectFolder.Text)
+        End If
+    End Sub
+
+    Sub enableBackNextButtons()
+        btnNext.Enabled = True
+        btnBack.Enabled = True
+
+        If (g_currentPage <= 1) Then
+            btnBack.Enabled = False
+        End If
+
+        If (g_currentPage >= g_tutorialPages.GetUpperBound(0)) Then
+            btnNext.Enabled = False
+        End If
+    End Sub
+
+    Sub displayCurrentTutorialPage()
+        lblCurPage.Text = "Page " & g_currentPage & " of " & g_tutorialPages.Count
+        webTutorial.Navigate(g_tutorialPages(g_currentPage - 1))
+        enableBackNextButtons()
+    End Sub
+
+    Function getTutorialFolder(xmlFilePath As String) As String
+        Dim toReturn As String = "NA"
+        Dim reader As XmlTextReader = New XmlTextReader(xmlFilePath)
+
+        reader.ReadToFollowing("TutorialName")
+        toReturn = reader.ReadElementContentAsString()
+
+        Return toReturn
+    End Function
+
+    Function getGMXFileName(inPath As String) As String
+        Dim toReturn As String = "NA"
+
+        'Brute force, get the last one always.
+        For Each foundFile As String In My.Computer.FileSystem.GetFiles(
+                                            inPath,
+                                            Microsoft.VisualBasic.FileIO.SearchOption.SearchTopLevelOnly,
+                                            "*.project.gmx")
+            toReturn = foundFile
+        Next
+
+        Return toReturn
+    End Function
+
+    Sub loadTutorialPages(tutorialPath As String)
+        Dim count As Integer = 0
+        For Each foundFile As String In My.Computer.FileSystem.GetFiles(
+                                            tutorialPath,
+                                            Microsoft.VisualBasic.FileIO.SearchOption.SearchTopLevelOnly,
+                                            "page*.html")
+            ReDim Preserve g_tutorialPages(count)
+            g_tutorialPages(count) = foundFile
+            count += 1
+        Next
+
+        If (g_tutorialPages.Count > 0) Then
+            Debug.Print("loading:  " + g_tutorialPages(0))            
+            g_currentPage = 1
+            displayCurrentTutorialPage()
+        Else
+            MessageBox.Show("Could not find any tutorial pages in " + tutorialPath)
+            g_currentPage = 0
+        End If
+
+    End Sub
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        loadEverything()
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        If (g_currentPage < g_tutorialPages.Count) Then
+            g_currentPage = g_currentPage + 1
+            displayCurrentTutorialPage()
+        End If
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        If (g_currentPage > 1) Then
+            g_currentPage = g_currentPage - 1
+            displayCurrentTutorialPage()
+        End If        
+    End Sub
+
+
+    Private Sub frmTutorialViewer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        enableBackNextButtons()
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Dim dirBrowser As FolderBrowserDialog
+        dirBrowser = New FolderBrowserDialog
+        dirBrowser.SelectedPath = txtProjectFolder.Text
+
+        If (dirBrowser.ShowDialog() = DialogResult.OK) Then
+            txtProjectFolder.Text = dirBrowser.SelectedPath
+            loadEverything()
+        End If
+    End Sub
+End Class
